5d28c83208c79a8a3044c3495c53967733e99062
Andrei Kun <andrei.kun@tss-yonder.com>
2015-10-05 11:58:04 +0300

    Add App facade

--- src/Jobs/GenericJob.php
+++ src/Jobs/GenericJob.php
@@ -6,7 +6,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Bus\SelfHandling;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Congredi\NotificationSystem\NotificationTypes\Interfaces\NotificationTypeInterface;
-use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Support\Facades\App;
 
 class GenericJob implements SelfHandling, ShouldQueue
 {
